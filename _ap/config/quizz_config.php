ecf002a682366f7d045f4241f34ae1e9cb6ae715
Boissier Florian <boissierflorian@outlook.com>
2018-03-19 17:40:23 +0100

    refacto(config) : separate app config

---
+++ _ap/config/quizz_config.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+/*
+|--------------------------------------------------------------------------
+| Navigation bar links
+|--------------------------------------------------------------------------
+|
+| Static Navigation links..
+|
+*/
+$config['navbar_links'] = array(
+array('name' => 'Courses', 'link' => '/courses'),
+array('name' => 'Quizz', 'link' => '/quizz')
+);
+
+/*
+|--------------------------------------------------------------------------
+| Stylesheets Paths
+|--------------------------------------------------------------------------
+|
+| CSS files paths.
+|
+*/
+$config['stylesheets'] = array(
+array('link' => 'w3'),
+array('link' => 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'),
+array('link' => 'common')
+);
+
+
+/*
+|--------------------------------------------------------------------------
+| Javascript Paths
+|--------------------------------------------------------------------------
+|
+| JS files paths.
+|
+*/
+$config['scripts'] = array(
+array('link' => 'common'),
+);
