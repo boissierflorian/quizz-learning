56bdd68a57b76b6e12908571ab966b0cd6adf8f2
Boissier Florian <boissierflorian@outlook.com>
2018-03-23 22:11:58 +0100

    view(success page): added content

--- _ap/views/register/success.php
+++ _ap/views/register/success.php
@@ -1,2 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<h1>{pagetitle}</h1>
+
+<div class="w3-panel w3-green">
+    <p>Votre inscription a été validée !</p>
+    <p>Redirection en cours <i class="fa fa-spinner w3-spin"></i></p>
+</div>
+
+<script type="text/javascript">
+    window.setTimeout(function(){
+        window.location.href = "<?= base_url('login') ?>";
+    }, 3000);
+</script>
